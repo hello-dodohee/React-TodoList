b19c313199a8438fedddb96710efd43d05a74b43
hello-dodohee <73160509+hello-dodohee@users.noreply.github.com>
2021-04-11 15:03:03 +0900

    Add 플러스버튼 누르면 할일 목록 추가되는 기능구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Template from "./components/Template";
 import TodoList from "./components/TodoList";
+import TodoInsert from "./components/TodoInsert";
 import { MdAddCircle } from "react-icons/md";
 import "./App.scss";
 
+let nextId = 4;
+
 function App() {
+  const [insertToggle, setInsertToggle] = useState(false);
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -26,19 +30,41 @@ function App() {
       text: "할일 4",
       checked: false,
     },
-    {
-      id: 5,
-      text: "할일 5",
-      checked: true,
-    },
   ]);
+
+  const onInsertToggle = () => {
+    setInsertToggle((prev) => !prev);
+  };
+
+  const onInsertTodo = (text) => {
+    if (text === "") {
+      return alert("할 일을 입력해주세요");
+    } else {
+      const todo = {
+        id: nextId,
+        text,
+        checked: false,
+      };
+      //push를 사용하면 해당 배열 자체가 변경되고
+      //concat을 사용하면 새 배열이 리턴되고 기존배열은 변경되지 않음.
+      setTodos((todos) => todos.concat(todo));
+      nextId++;
+    }
+  };
+
   return (
     <div className="App">
       <Template todoLength={todos.length}>
         <TodoList todos={todos} />
-        <div className="add-todo-button">
+        <div className="add-todo-button" onClick={onInsertToggle}>
           <MdAddCircle />
         </div>
+        {insertToggle && (
+          <TodoInsert
+            onInsertToggle={onInsertToggle}
+            onInsertTodo={onInsertTodo}
+          />
+        )}
       </Template>
     </div>
   );
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,1 +1,36 @@
+import React, { useState } from "react";
+import { MdAddCircle } from "react-icons/md";
+import "./TodoInsert.scss";
 
+const TodoInsert = ({ onInsertToggle, onInsertTodo }) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault(); //자동으로 새로고침 되는것을 막아주는 함수
+    onInsertTodo(value);
+    setValue("");
+    onInsertToggle();
+  };
+
+  return (
+    <div>
+      <div className="background" onClick={onInsertToggle}></div>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="오늘의 할일은 무엇인가요?"
+          value={value}
+          onChange={onChange}
+        ></input>
+        <button type="submit">
+          <MdAddCircle />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoInsert;
